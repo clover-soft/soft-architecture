fdb15670ee0b4a1662e06c6b8948d0cf3635298b
Yakov <yakov@panargin.ru>
2023-09-03 20:12:42 +0300

    code review changes

--- lesson04/src/TicketService/TicketValidator.java
+++
@@ -1,8 +0,0 @@
-package TicketService;
-import TicketService.TicketClasses.Ticket;
-public class TicketValidator {
-    public boolean validateTicket(Ticket ticket) {
-        // Логика для проверки валидности билета
-        return ticket.isValid();
-    }
-}
